d7592d380c7cd6bdc980e2536fa16d685b7d4e48
dshilov <dunderflute@yandex.ru>
2020-08-09 06:42:35 +0300

    Refactoring

--- src/main/java/ru/shilov/cc/currencyconverter/util/ValueDeserializer.java
+++ src/main/java/ru/shilov/cc/currencyconverter/util/ValueDeserializer.java
@@ -1,7 +1,6 @@
 package ru.shilov.cc.currencyconverter.util;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
@@ -10,8 +9,8 @@ import java.io.IOException;
 public final class ValueDeserializer extends JsonDeserializer<Double> {
 
     @Override
-    public Double deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        String value = jsonParser.readValueAs(String.class);
+    public Double deserialize(final JsonParser jsonParser, final DeserializationContext deserializationContext) throws IOException {
+        final String value = jsonParser.readValueAs(String.class);
         return Double.parseDouble(value.replace(",", "."));
     }
 
